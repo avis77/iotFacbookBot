ef39c3728a4c73cd96f629fce68e3b23f3eba931
avis77 <aviram.sht@gmail.com>
2017-02-28 20:09:54 +0200

    change versions and add req

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+var request = require('request');  
+
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
