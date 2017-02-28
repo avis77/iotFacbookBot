b1d84987812e299632f9940bef89942f7a57be16
avis77 <aviram.sht@gmail.com>
2017-02-28 20:07:58 +0200

    change to false

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 /* For Facebook Validation */
 app.get('/webhook', (req, res) => {
