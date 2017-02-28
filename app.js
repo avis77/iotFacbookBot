48a0d3366dd7feb3a8a5371ea1e0b6e7d32bb2e2
avis77 <aviram.sht@gmail.com>
2017-02-28 20:37:58 +0200

    remoove url

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 var request = require('request');  
 
 const app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use(bodyParser.json());
 
 /* For Facebook Validation */
