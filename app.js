b5335fed59fd19e246a6184ed79e287ba1ed20a6
avis77 <aviram.sht@gmail.com>
2017-03-01 20:47:38 +0200

    change respond msgs and log

--- app.js
+++ app.js
@@ -16,24 +16,45 @@ function verificationHandler(req, res) {
 }
 
 /* Handling all messenges */
-app.post('/webhook', (req, res) => {
-  var msg = "";
-  if (req.body.object === 'page') {
-	
-    req.body.entry.forEach((entry) => {
-      entry.messaging.forEach((event) => {
-		  console.log("event "+event);
-        if (event.message && event.message.text) {
-          msg = msg+event.message.text+".";
+app.post('/webhook',, receiveMessage);
+function receiveMessage(req, res, next) {
+    var message_instances = req.body.entry[0].messaging;
+    message_instances.forEach(function(instance){
+        var sender = instance.sender.id;
+        if(instance.message && instance.message.text) {
+            var msg_text = instance.message.text;
+			console.log(sender +" : "+msg_text);
+            sendMessage(sender, msg_text, true);
         }
-      });
     });
-	console.log("msg "+msg);
-	
-	res.send(msg);
-    res.status(200).end();
-  }
-});
+    res.sendStatus(200);
+}
+
+function sendMessage(receiver, data, isText) {
+    var payload = {};
+    payload = data;
+
+    if(isText) {
+        payload = {
+            text: data
+        };
+    }
+
+    request({
+        url: conf.FB_MESSAGE_URL,
+        method: 'POST',
+        qs: {
+            access_token: process.env.PROFILE_TOKEN || conf.PROFILE_TOKEN
+        },
+        json: {
+            recipient: {id: receiver},
+            message: payload
+        }
+    }, function (error, response) {
+        if(error) console.log('Error sending message: ', error);
+        if(response.body.error) console.log('Error: ', response.body.error);
+    });
+}
 app.get('/', (req, res) => {
   res.writeHead(200, {"Content-Type": "text/plain"});
   res.end("Hello World\n");
